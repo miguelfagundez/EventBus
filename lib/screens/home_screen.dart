0f296f56b99c32ef50135b58473bc0428ba6f17e
Miguel Fagundez <59711011+miguelfagundez@users.noreply.github.com>
2022-11-22 16:43:08 -0500

    gitignore

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,8 +8,18 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Event Bus'),
+        elevation: 2,
       ),
       body: const Center(child: Text('My Event Bus Example')),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+              child: const Icon(Icons.remove), onPressed: () {}),
+          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {})
+        ],
+      ),
     );
   }
 }
